dca8d7b84b87f0b19b152c8c021751aece1aaa2b
Alberto Castañeiras <alberto@casta.me>
2024-01-15 18:25:13 +0100

    Remove code

--- cli/homelab.go
+++ cli/homelab.go
@@ -3,7 +3,6 @@ package homelab
 import (
 	"log"
 
-	"github.com/urfave/cli/v2"
 	"github.com/vekio/homelab/cli/conf"
 	"github.com/vekio/homelab/cli/secrets"
 )
@@ -14,46 +13,3 @@ func init() {
 		log.Fatalf("failed to init secrets %s", err)
 	}
 }
-
-var testCmd = &cli.Command{
-	Name:  "test",
-	Usage: "testing",
-	Action: func(cCtx *cli.Context) error {
-
-		return nil
-	},
-}
-
-// var initCmd = &cli.Command{
-// 	Name:    "init",
-// 	Aliases: []string{"i"},
-// 	Usage:   "Initialize required folders and config files",
-// 	Action: func(cCtx *cli.Context) error {
-// 		service := utils.ParentCommandName(cCtx)
-// 		repository := settings.getRepository()
-
-// 		envConfig := fmt.Sprintf("%s/%s", conf.Config.DirPath(), service)
-// 		repoConfig := fmt.Sprintf("%s/%s/config", repository, service)
-
-// 		switch service {
-// 		case s.AUTHELIA:
-// 			if err := s.InitAuthelia(repoConfig, envConfig); err != nil {
-// 				return err
-// 			}
-// 		case s.GITEA:
-// 			if err := s.InitGitea(envConfig); err != nil {
-// 				return err
-// 			}
-// 		case s.LLDAP:
-// 			if err := s.InitLldap(envConfig); err != nil {
-// 				return err
-// 			}
-// 		case s.TRAEFIK:
-// 			if err := s.InitTraefik(repoConfig, envConfig); err != nil {
-// 				return err
-// 			}
-// 		}
-
-// 		return nil
-// 	},
-// }
