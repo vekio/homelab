1cf21ecd9abd8b5ccebb931a5f3faef23bd78517
Alberto Castañeiras <alberto@casta.me>
2024-12-15 22:28:52 +0100

    Remove whoami

--- infra/dns_cname_records.tf
+++ infra/dns_cname_records.tf
@@ -13,11 +13,6 @@ resource "adguard_rewrite" "traefik_int_record" {
   answer = "oberon.home.local"
 }
 
-resource "adguard_rewrite" "whoami_record" {
-  domain = "whoami.${var.domain}"
-  answer = "proxy.home.local"
-}
-
 resource "adguard_rewrite" "status_record" {
   domain = "status.${var.domain}"
   answer = "proxy.home.local"
