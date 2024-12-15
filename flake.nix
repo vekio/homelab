d59cbeeb5a0c0826d2d6149148c0d6042d3d090b
Alberto Castañeiras <alberto@casta.me>
2024-12-15 23:58:48 +0100

    update flake

--- flake.nix
+++ flake.nix
@@ -5,28 +5,28 @@
 
   outputs = { self, nixpkgs }:
     let
-      goVersion = 22; # Change this to update the whole stack
 
       supportedSystems =
         [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+
       forEachSupportedSystem = f:
         nixpkgs.lib.genAttrs supportedSystems (system:
           f {
             pkgs = import nixpkgs {
               inherit system;
               config = { allowUnfree = true; };
-              overlays = [ self.overlays.default ];
             };
           });
     in {
-      overlays.default = final: prev: {
-        go = final."go_1_${toString goVersion}";
-      };
-
       devShells = forEachSupportedSystem ({ pkgs }: {
         default = pkgs.mkShell {
           name = "homelab";
+
           packages = with pkgs; [ ansible ansible-lint terraform ];
+
+          env = { };
+
+          shellHook = "";
         };
       });
     };
